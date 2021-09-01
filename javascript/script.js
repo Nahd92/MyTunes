fb130512216338e393647cbb798cd6cc0f9951d3
Nahd92 <dhan.marcellus@hotmail.com>
2021-09-01 14:33:42 +0200

    some bugs but all in all almsot done

--- javascript/script.js
+++ javascript/script.js
@@ -10,16 +10,16 @@ const sidebarArrow = document.querySelector('.sidebar__arrow')
 const arrowleft = document.getElementById("arrow-left");
 
 const searchIcon = document.getElementById("nav-search"),
-		 searchContainer = document.getElementById("search"),
+		 searchContainer = document.querySelector(".search-container"),
 			searchSection = document.getElementById("search-section");
 
 const searchInput = document.getElementById('search-input');
 const searchCross = document.getElementById("cross-icon");
 const form = document.getElementById('add-track');
 
-const editButtons = document.querySelectorAll('.edit-btn')
-const removeButtons = document.querySelectorAll('.remove-btn')
-const changeBtn = document.querySelectorAll('.change-btn');
+const editButtons = Array.from(document.querySelectorAll('.edit-btn'))
+const removeButtons = Array.from(document.querySelectorAll('.remove-btn'))
+const changeBtn = Array.from(document.querySelectorAll('.change-btn'));
 
 
 /* === STORED VALUES  ==== */
@@ -57,36 +57,31 @@ function openSearchContainer() {
 
 
 function checkIfSearchWordExist(e) {
-	const noResult = document.querySelector('.noResultAfterSearch');
 	const trackCard = Array.from(document.querySelectorAll('.track-card .name'));
-
-
 	let fetchedArtistName = trackCard.filter(a => a.innerHTML.toLowerCase() === e.target.value.toLowerCase());
 
 		for (let i = 0; i <= fetchedArtistName.length; i++) {
 			const a = fetchedArtistName[i];
-
 			if(fetchedArtistName.length > 0){
 				let superParent = a.parentElement.parentElement;
 				let parents = a.parentElement;
-	
-				console.log(a.innerHTML);
+
 				const img = superParent.children[0].getAttribute('src');
-				const overlay = superParent.children[1].textContent;
+				const overlay = superParent.children[1].children[0].getAttribute('src');
 				
 				const artistName = parents.children[1].textContent;
 				const genreName = parents.children[3].textContent;
 				const releaseYear = parents.children[5].textContent;
 				const albumName = parents.children[7].textContent;
-				
-				console.log(parents.children[1].textContent);
-				console.log(genreName);
-				console.log(releaseYear);
-				console.log(parents.children[7].textContent);
-			  createCard("search", "search-container", artistName, genreName,releaseYear,albumName, img, overlay)
+
+
+				createCard(superParent.id, "search", "search-container", artistName, genreName,releaseYear,albumName, img, overlay)
 			}
 			else {
-				noResult.innerHTML = "No track in list";
+				const createDiv = document.createElement('div');
+				createDiv.innerHTML = "No tracks were found"; 
+				createDiv.setAttribute('style', 'margin-top: 2em; text-align: center; font-size: 2em;' )
+			  	searchContainer.appendChild(createDiv);
 			}
 		}
 }
@@ -129,7 +124,7 @@ function sidebarMoveWithHiddenContainer() {
 
 /* === CREATE ELEMENT   ==== */
 
-function createCard(section, container, artist, artistGenre, 
+function createCard(id, section, container, artist, artistGenre, 
 	releasedYear, album, imageFilename, trackFilename) {
 
 	const cardClassName	= "track-card";
@@ -139,6 +134,7 @@ function createCard(section, container, artist, artistGenre,
 
 	let card = document.createElement('div');
 	card.className = cardClassName
+	card.id = id;
 
 	let overlay = createTrackAndOverlayInfo(trackFilename);
 	const artistInfo = createArtistInfo(artist);
@@ -147,6 +143,10 @@ function createCard(section, container, artist, artistGenre,
 	const albumInfo = createAlbumnInfo(album);
 	const cardInfo = createCardInfo()
 	const image = createImgElement(imageFilename);
+	const editCardContainer = createEditCardContainer();
+	const cardButtons = createCardButtons();
+	const editContainer = createContainer();
+
 
 	newTracksSection.appendChild(newtrackcontainer);
 	newtrackcontainer.appendChild(card)
@@ -162,6 +162,113 @@ function createCard(section, container, artist, artistGenre,
 	cardInfo.appendChild(releaseInfo[1])
 	cardInfo.appendChild(albumInfo[0])
 	cardInfo.appendChild(albumInfo[1])
+
+	editContainer.appendChild(editCardContainer);
+
+	card.appendChild(editContainer)
+	card.appendChild(cardButtons);
+	
+}
+
+function createContainer() {
+	let container = document.createElement('div');
+	container.className = "edit-card-container";
+	return container;
+}
+
+function createEditCardContainer() {
+	let editCards = document.createElement('div');
+	editCards.className = "edit-cards";
+
+	let editArtistName = document.createElement('h4')
+	editArtistName.className = "edit-artistname";
+	editArtistName.innerHTML = 'Change Artist:'
+	let editArtistInput = document.createElement('input')
+	editArtistInput.className = "edit-artist-name"
+	editArtistInput.setAttribute('type', 'text')
+	
+	let editGenre = document.createElement('h4')
+	editGenre.className = "edit-artistname";
+	editGenre.innerHTML = 'Change Genre:'
+	let editArtistGenreInput = document.createElement('input')
+	editArtistGenreInput.className = "edit-artist-genre"
+	editArtistGenreInput.setAttribute('type', 'text')
+
+
+	let editArtistRelease = document.createElement('h4')
+	editArtistRelease.className = "edit-artistname";
+	editArtistRelease.innerHTML = 'Change: release:'
+	
+	let editArtistReleaseInput = document.createElement('input')
+	editArtistReleaseInput.className = "edit-artist-release"
+	editArtistReleaseInput.setAttribute('type', 'text')
+
+	let editArtistAlbum = document.createElement('h4')
+	editArtistAlbum.className = "edit-artistname";
+	editArtistAlbum.innerHTML = 'Change: album:'
+	let editArtistAlbumInput = document.createElement('input')
+	editArtistAlbumInput.className = "edit-artist-album"
+	editArtistAlbumInput.setAttribute('type', 'text')
+
+	editCards.appendChild(editArtistName)
+	editCards.appendChild(editArtistInput)
+	editCards.appendChild(editGenre)
+	editCards.appendChild(editArtistGenreInput)
+	editCards.appendChild(editArtistRelease)
+	editCards.appendChild(editArtistReleaseInput)
+	editCards.appendChild(editArtistAlbum)
+	editCards.appendChild(editArtistAlbumInput)
+
+	return editCards;
+}
+
+function createCardButtons() {
+	const cardButton = document.createElement('div')
+	cardButton.className = "card-buttons"
+
+	const removeAref = document.createElement('a')
+	removeAref.className = "remove-btn track-button";
+	removeAref.setAttribute("href", "#")
+	removeAref.textContent = "Remove"
+
+	removeAref.addEventListener('click', (e) => {
+		e.preventDefault();
+		removeACard(e);
+	})
+
+
+	
+	const editAref = document.createElement('a')
+	editAref.className = "edit-btn track-button"
+	editAref.setAttribute("href", "#")
+	editAref.textContent = "Edit"
+	
+	editAref.addEventListener('click', (event) => {
+		event.preventDefault();
+		removeEditButtonOnClick(event);
+		addChangeButtonOnClick(event);
+		editACard(event);
+	})
+
+
+	const changeAref = document.createElement('a')
+	changeAref.className = "change-btn track-button";
+	changeAref.setAttribute("href", "#")
+	changeAref.textContent = "Change Values"
+	changeAref.addEventListener('click', (e) => {
+		e.preventDefault();
+		editACard(e);
+		removeEditButtonOnClick(e);
+		addChangeButtonOnClick(e);
+		acceptChanges(e);
+	})
+
+
+	cardButton.appendChild(removeAref)
+	cardButton.appendChild(editAref)
+	cardButton.appendChild(changeAref)
+	
+	return cardButton;
 }
 
 function createTrackAndOverlayInfo(trckFile) {
@@ -173,7 +280,7 @@ function createTrackAndOverlayInfo(trckFile) {
 
 	let trackFile = document.createElement('audio');
 	trackFile.setAttribute('src', `${trckFile}`);
-	trackFile.classList = trackfileNameClassName
+	trackFile.className = trackfileNameClassName
 	trackFile.controls = true;
 
 	 overlay.appendChild(trackFile)
@@ -253,7 +360,7 @@ function createCardInfo() {
 }
 
 
-function formValuesOnSubmitAndCreateCard() {
+function formValuesOnSubmitAndCreateCard(e) {
 	const artistInput = form.elements["artist"].value;
 	const genreInput = form.elements["genre"].value;
 	const release = form.elements["release"].value;
@@ -264,7 +371,7 @@ function formValuesOnSubmitAndCreateCard() {
 	const trackCoverImgURL = URL.createObjectURL(profileImg);
 	const trackFileAudioURL = URL.createObjectURL(profileAud);
 
-	createCard("added-tracks", "addded-tracks-container" ,artistInput, genreInput,release, recentAlbum, trackCoverImgURL, trackFileAudioURL);
+	createCard(e ,"added-tracks", "addded-tracks-container" ,artistInput, genreInput,release, recentAlbum, trackCoverImgURL, trackFileAudioURL);
 }
 
 
@@ -273,50 +380,54 @@ function clearSearchInputField() {
 }
 
 function removeAllCardsInSearchContainer() {
-	const firstChild = searchContainer.firstElementChild;
 	searchContainer.innerHTML = '';
-	searchContainer.append(firstChild);
 }
 
 
 function removeACard(event) {
+	console.log(event)
 	let parentsParent = event.target.parentElement.parentElement;
 	parentsParent.parentNode.removeChild(parentsParent);  
 }
 
+
+
 function editACard(event) {
 	let parentsParent = event.target.parentElement.parentElement;
-	let editCards= parentsParent.children[3].children[0];
 
-	// console.log(parentsParent)
-	// console.log(editCards)
-	
-	let artist = editCards.children[1].value;
-	let genre =  editCards.children[3].value;
-	let year =   editCards.children[5].value;
-	let album =  editCards.children[7].value;
-	
-
+	let editCards= parentsParent.children[3];
+	editCards.classList.toggle('active')
 }
 
 function acceptChanges(event) {
 	let parentsParent = event.target.parentElement.parentElement;
-	let editCards= parentsParent.children[3];
-	
+	console.log(parentsParent)
+	let cardInfo = parentsParent.children[2];
+	let editCards= parentsParent.children[3].children[0];
+	console.log(editCards)
+
+		
+	cardInfo.children[1].innerHTML = editCards.children[1].value;
+	cardInfo.children[3].innerHTML =  editCards.children[3].value;
+	cardInfo.children[5].innerHTML =   editCards.children[5].value;
+	cardInfo.children[7].innerHTML =  editCards.children[7].value;
 }
 
 function removeEditButtonOnClick(e) {
-	let parentsParent = e.target.parentElement.parentNode.id;
+	console.log(e)
+	const parentsParent = e.target.parentElement.parentNode.id;
+	console.log(parentsParent)
 	const getCardTrackWithId = document.getElementById(parentsParent);
-	let editButton = getCardTrackWithId.children[4].children[1]
-	console.log(editButton)
+
+	console.log(getCardTrackWithId)
+	const editButton = getCardTrackWithId.children[4].children[1]
 	editButton.classList.toggle('active');
 }
 
 function addChangeButtonOnClick(e) {
-	let parentsParent = e.target.parentElement.parentNode.id;
+	const parentsParent = e.target.parentElement.parentNode.id;
 	const getCardTrackWithId = document.getElementById(parentsParent);
-	let changeButton = getCardTrackWithId.children[4].children[2]
+	const changeButton = getCardTrackWithId.children[4].children[2]
 	changeButton.classList.toggle('active');
 }
 
@@ -326,7 +437,8 @@ function addChangeButtonOnClick(e) {
 /* === EVENT LISTNERS  ==== */
 window.addEventListener('scroll', () => {
 	let top = window.scrollY;
-	sidebarSwapColor(top);
+	let width = window.scrollX;
+	sidebarSwapColor(top,width);
 });
 
 toggle.addEventListener('click', () => {
@@ -337,7 +449,6 @@ toggle.addEventListener('click', () => {
 
 menuToggle.addEventListener('click', () => {
 	showMenu()
-
 });
 
 searchIcon.addEventListener('click', () => {
@@ -358,8 +469,8 @@ searchInput.addEventListener('keyup',  (e) => {
 
 
 searchCross.addEventListener('click', () => {
-	removeAllCardsInSearchContainer();
-	closeSearchContainer()
+removeAllCardsInSearchContainer();
+closeSearchContainer() 
 });
 
 
@@ -375,9 +486,9 @@ for (let i = 0; i < editButtons.length; i++) {
 	const element = editButtons[i];
 	element.addEventListener('click', function(event)  {
 		event.preventDefault();
-		editACard(event);
 		removeEditButtonOnClick(event);
 		addChangeButtonOnClick(event);
+		editACard(event);
 	})
 }
 
@@ -394,8 +505,9 @@ for (let i = 0; i < changeBtn.length; i++) {
 	const element = changeBtn[i];
 	element.addEventListener('click', (e) => {
 		e.preventDefault();
-		acceptChanges(e);
+		editACard(e);
 		removeEditButtonOnClick(e);
 		addChangeButtonOnClick(e);
+		acceptChanges(e);
 	})
 }
